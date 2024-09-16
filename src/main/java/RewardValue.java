d7c8fd11e78aa5da34d56b52430326a71a3f6da1
Ganesh.v <155699230+GANESHGITHUB112@users.noreply.github.com>
2024-09-16 10:49:39 +0530

    Update RewardValue.java

--- src/main/java/RewardValue.java
+++ src/main/java/RewardValue.java
@@ -1,2 +1,37 @@
 public class RewardValue {
+    private double cashValue;
+    private int milesValue;
+    private static final double MILES_TO_CASH_CONVERSION_RATE = 0.0035;
+
+    // Constructor for cash value
+    public RewardValue(double cashValue) {
+        this.cashValue = cashValue;
+        this.milesValue = convertCashToMiles(cashValue);
+    }
+
+    // Constructor for miles value
+    public RewardValue(int milesValue) {
+        this.milesValue = milesValue;
+        this.cashValue = convertMilesToCash(milesValue);
+    }
+
+    // Get cash value
+    public double getCashValue() {
+        return cashValue;
+    }
+
+    // Get miles value
+    public int getMilesValue() {
+        return milesValue;
+    }
+
+    // Convert cash to miles
+    private int convertCashToMiles(double cashValue) {
+        return (int) (cashValue / MILES_TO_CASH_CONVERSION_RATE);
+    }
+
+    // Convert miles to cash
+    private double convertMilesToCash(int milesValue) {
+        return milesValue * MILES_TO_CASH_CONVERSION_RATE;
+    }
 }
